3d90c38e4fc23bc8fb20f4738dd696272006d4d6
Jake Olaveson <jakeolaveson@gmail.com>
2017-01-13 18:06:47 -0700

    Updated notes

--- include/Notes.c
+++ include/Notes.c
@@ -1,12 +1,20 @@
 /*
 	This is the start when turning computer on
 
+	Set Up
+
 	You don't need to follow in exact order, but it is there for convenience in time
 	Step 1: Open up Sublime Text  (it should already be open)
 	Step 2: Open up GitBash
 	Step 3: Open up Docker
 	Step 4: Open up GitHub
-	Step 5: In Docker and GitBash: Run cd Documents/Git-Hub/starstruck-convex
-	Step 6: In Docker: Run make windocker
+	Step 5: In Docker and GitBash: Run cd Documents/Git-Hub/starstruck-convex (when typing click tab to have it finish for you)
+	Step 6: In Docker: Run make windocker (once Docker is ready)
 
+	After you finish Programming
+
+	when pushing files into GitHub run the following steps in GitBash:
+		1- git add .
+		2- git commit -m "Type what you did today"
+		3- git push origin master
 */
