596c948da2d291f797b70b7a1cd1942e5ac5f41b
Jake Olaveson <jakeolaveson@gmail.com>
2017-01-13 17:54:59 -0700

    made Notes file

---
+++ include/Notes.c
@@ -0,0 +1,12 @@
+/*
+	This is the start when turning computer on
+
+	You don't need to follow in exact order, but it is there for convenience in time
+	Step 1: Open up Sublime Text  (it should already be open)
+	Step 2: Open up GitBash
+	Step 3: Open up Docker
+	Step 4: Open up GitHub
+	Step 5: In Docker and GitBash: Run cd Documents/Git-Hub/starstruck-convex
+	Step 6: In Docker: Run make windocker
+
+*/
